c6730cbe4cb485e2961c7748ea19962d69a9e560
Rahul Sharma <rahulrs0029@gmail.com>
2022-03-05 16:19:17 +0530

    Initial Commit

---
+++ SegmentedControlSwiftUI/SegmentedControlSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlSwiftUIApp.swift
+//  SegmentedControlSwiftUI
+//
+//  Created by Rahul Sharma on 05/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
